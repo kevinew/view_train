e587c789cb701eb9fbe92ae1d8985bf86197c543
Wenkai Liu <kevinew.1221@gmail.com>
2014-03-30 08:38:30 -0700

    segment error.

--- drawing_skylines.cc
+++ drawing_skylines.cc
@@ -4,22 +4,38 @@
 
 // Description:
 //   窗外有很多矩形建筑物，站在窗口用相机拍一张照片，求出照片中许多建筑物重叠后出现的锯齿状的外形轮廓。
-//   Analyze:
+// Analyze:
 //   Using the binary tree or segment tree to solve it.
+// OJ:  http://codeforces.com/contest/35/problem/E
 
+#include<stdio.h>
+#include<vector>
 #include<iostream>
 
 using namespace std;
 
+typedef struct Point {
+  int h, x;
+} POINT;
+vector<POINT> surface;
+
 struct Node {
   int h, x, y;
   Node *left, *right;
-} root;
+} *root;
 
-void UpdateTree(Node * r, Node* line) {
-  if (NULL == line || line.y - line.x == 0) return;
-  if (NULL == r) *r = line;
-
+void UpdateTree(Node* r, Node* line) {
+  cout << "update tree.1" << endl;
+  if (NULL == line || line->y - line->x == 0) return;
+  cout << "update tree.2" << endl;
+  if (NULL == r) {
+    // *r = *line;
+    cout << "update tree.3" << endl;
+    *r = *line;
+    cout << "update tree.4" << endl;
+    return;
+  }
+  cout << "update tree." << endl;
   // 两条线段左右两端完全对qi的情况
   if (r->x == line->x && r->y == line->y ) {
     if (r->h < line->h) r->h = line->h;
@@ -49,7 +65,7 @@ void UpdateTree(Node * r, Node* line) {
     node->left = r->left;
     r->left = node;
     r->x = line->x;
-    updateTree(r, line);
+    UpdateTree(r, line);
   }
   // longer or shorter compared to r right point.
   if (line->y > r->y) {
@@ -65,7 +81,7 @@ void UpdateTree(Node * r, Node* line) {
     node->right = r->right;
     r->right = node;
     r->y = line->y;
-    updateTree(r, line);
+    UpdateTree(r, line);
   }
 
 }
@@ -73,32 +89,70 @@ void UpdateTree(Node * r, Node* line) {
 
 void Insert(Node line) {
   Node *building = new Node(line);
-  UpdateTree(r, building);
+  UpdateTree(root, building);
+  cout << "Insert done." << endl;
 }
 
-void Output(Node *r) {
+void SetResult(Node *r) {
   if (NULL == r) return;
-  Output(r->left);
-  cout << r->x << " " << r->y << " " << r->h << endl;
-  Output(r->right);
+
+  SetResult(r->left);
+  POINT point;
+  point.x = r->x;
+  point.h = r->h;
+  surface.push_back(point);
+  // cout << r->x << " " << r->y << " " << r->h << endl;
+  SetResult(r->right);
 }
 
-void Run(const vector<Node> & building) {
-  for (int i = 0; i < building.size(); ++i) {
-    Insert(building[i]);
+void Run(vector<Node> buildings) {
+  for (int i = 0; i < buildings.size(); ++i) {
+    Insert(buildings[i]);
   }
 
-  Output(r);
-  return;
+  cout << "Run.1" << endl;
+  SetResult(root);
+  if (surface.front().h > 0) {
+    POINT point;
+    point.x = surface.front().x;
+    point.h = surface.front().h;
+    surface.insert(surface.begin(), point);
+  }
+  if (surface.back().h > 0) {
+    POINT point;
+    point.x = surface.back().x;
+    point.h = surface.back().h;
+    surface.push_back(point);
+  }
+
+  cout << surface.size() << endl;
+  for (int i = 0; i < surface.size(); ++i) {
+    cout << surface[i].x << " " << surface[i].h << endl;
+  }
 }
 
 int main(int argc, char **argv) {
-  vector<Node> building;
+  vector<struct Node> buildings;
   
   // TODO(wenkailiu): initialize building
-
-  Run(building);
+  int n;
+  while(scanf("%d", &n) != EOF) {
+    for (int i = 0; i < n; ++i) {
+      Node building;
+      scanf("%d%d%d",&building.h, &building.x, &building.y);
+      if (building.x > building.y) {
+        int tmp = building.x;
+        building.x = building.y;
+        building.y = tmp;
+      }
+      buildings.push_back(building);
+    }
+    for (int i = 0; i < buildings.size(); ++i) {
+      cout << "building:" << buildings[i].x << " " << buildings[i].y << " "
+        << buildings[i].h << endl;
+    }
+    Run(buildings);
+  }
 
   return 0;
 }
-
